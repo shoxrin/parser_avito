9ff555ffda2a4fb018a68da16cf8c7bd9236c257
Шохрин Кирилл <59385807+shoxrin@users.noreply.github.com>
2021-10-06 19:03:45 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,101 @@
+URLS = {
+    'СПБ': {
+        'до 300к': 'https://www.avito.ru/sankt_peterburg_i_lo/avtomobili?pmax=300000&s=104',
+        '300-700': 'https://www.avito.ru/sankt_peterburg_i_lo/avtomobili?pmin=300000&pmax=700000&s=104',
+        '700-1,5млн': 'https://www.avito.ru/sankt_peterburg_i_lo/avtomobili?pmin=700000&pmax=1500000&s=104',
+        '1,5млн>': 'https://www.avito.ru/sankt_peterburg_i_lo/avtomobili?pmin=1500000&s=104'
+    },
+    'Архангельск': {
+        'до 300к': 'https://www.avito.ru/arhangelsk/avtomobili?pmax=300000&s=104',
+        '300-700': 'https://www.avito.ru/arhangelsk/avtomobili?pmin=300000&pmax=700000&s=104',
+        '700-1,5млн': 'https://www.avito.ru/arhangelsk/avtomobili?pmin=700000&pmax=1500000&s=104',
+        '1,5млн>': 'https://www.avito.ru/arhangelsk/avtomobili?pmin=1500000&s=104'
+    },
+    'МСК': {
+        'до 300к': 'https://www.avito.ru/moskva_i_mo/avtomobili?pmax=300000&s=104',
+        '300-700': 'https://www.avito.ru/moskva_i_mo/avtomobili?pmin=300000&pmax=700000&s=104',
+        '700-1,5млн': 'https://www.avito.ru/moskva_i_mo/avtomobili?pmin=700000&pmax=1500000&s=104',
+        '1,5млн>': 'https://www.avito.ru/moskva_i_mo/avtomobili?pmin=1500000&s=104'
+    },
+    'Ростов-на-дону': {
+        'до 300к': 'https://www.avito.ru/rostov-na-donu/avtomobili?pmax=300000&s=104',
+        '300-700': 'https://www.avito.ru/rostov-na-donu/avtomobili?pmin=300000&pmax=700000&s=104',
+        '700-1,5млн': 'https://www.avito.ru/rostov-na-donu/avtomobili?pmin=700000&pmax=1500000&s=104',
+        '1,5млн>': 'https://www.avito.ru/rostov-na-donu/avtomobili?pmin=1500000&s=104'
+    },
+    'Самара': {
+        'до 300к': 'https://www.avito.ru/samara/avtomobili?pmax=300000&s=104',
+        '300-700': 'https://www.avito.ru/samara/avtomobili?pmin=300000&pmax=700000&s=104',
+        '700-1,5млн': 'https://www.avito.ru/samara/avtomobili?pmin=700000&pmax=1500000&s=104',
+        '1,5млн>': 'https://www.avito.ru/samara/avtomobili?pmin=1500000&s=104'
+    },
+    'Омск': {
+        'до 300к': 'https://www.avito.ru/omsk/avtomobili?pmax=300000&s=104',
+        '300-700': 'https://www.avito.ru/omsk/avtomobili?pmin=300000&pmax=700000&s=104',
+        '700-1,5млн': 'https://www.avito.ru/omsk/avtomobili?pmin=700000&pmax=1500000&s=104',
+        '1,5млн>': 'https://www.avito.ru/omsk/avtomobili?pmin=1500000&s=104'
+    },
+    'ЕКБ': {
+        'до 300к': 'https://www.avito.ru/ekaterinburg/avtomobili?pmax=300000&s=104',
+        '300-700': 'https://www.avito.ru/ekaterinburg/avtomobili?pmin=300000&pmax=700000&s=104',
+        '700-1,5млн': 'https://www.avito.ru/ekaterinburg/avtomobili?pmin=700000&pmax=1500000&s=104',
+        '1,5млн>': 'https://www.avito.ru/ekaterinburg/avtomobili?pmin=1500000&s=104'
+    },
+    'Казань': {
+        'до 300к': 'https://www.avito.ru/kazan/avtomobili?pmax=300000&s=104',
+        '300-700': 'https://www.avito.ru/kazan/avtomobili?pmin=300000&pmax=700000&s=104',
+        '700-1,5млн': 'https://www.avito.ru/kazan/avtomobili?pmin=700000&pmax=1500000&s=104',
+        '1,5млн>': 'https://www.avito.ru/kazan/avtomobili?pmin=1500000&s=104'
+    },
+}
+
+WEBHOOK_URLS = {
+    'СПБ': {
+        'до 300к': 'https://discord.com/api/webhooks/895295379522850897/aSXGnNh0R9dPVZ0BgI-M4t2Q021XB3h5iR2W2tfc2NGNm1nmwJFRniQxgJj-Tl2IjdDj',
+        '300-700': 'https://discord.com/api/webhooks/895294828257083442/Z7ZfJN_wGTG6ArxioAfbddqKZYYQpdWG_a9eiR3QuVBBjvKSamxf53lcD-Wxbj85sdT5',
+        '700-1,5млн': 'https://discord.com/api/webhooks/895294741778956340/c-ff-wHIcYNBO4z1Ouc56kTfTjPEx7wJ-DBXKlu8AyaZPMWzbCmC1ACSVoylbOBaGlK1',
+        '1,5млн>': 'https://discord.com/api/webhooks/895294606546202644/b2-T8jGJNPW2KGmoxgG1-rdnkRdNV2IMtNyW_mBgPxtuch13mrG3HbmgUfkSot3pvkEV'
+    },
+    'Архангельск': {
+        'до 300к': 'https://discord.com/api/webhooks/895294523159228448/9zVhPlf2LO4P7b53CqNZCVeTDGZIhes_209QpBDsCLrfOKHV5d-7ECa-UrEY-KgyGSxU',
+        '300-700': 'https://discord.com/api/webhooks/895294405957783603/BOgLd3cWdMqy1syD2dxWHMqPa9I-C2-ZtDfELUyY8A3l3UGbKJVWLXzFVaACj0Zszgno',
+        '700-1,5млн': 'https://discord.com/api/webhooks/895294168144953375/jncX1o1FFdoyEDXPIzqKXb81jWNrieqiRMyYK6iSFIdhTNjMEo4n4JjOSCVSFew-bxFN',
+        '1,5млн>': 'https://discord.com/api/webhooks/895275318527529040/EgL2LmPMAcGfu4cDAyj-t5CUhEn_VYZ3qZQD0IAIKylxmPUsb3JQXYi4CugQfEwaRzsg'
+    },
+    'МСК': {
+        'до 300к': 'https://discord.com/api/webhooks/895275115472887808/08NleXFVdeeSgVrELKcYxiRKZoLyL9-5qVxYCQgyUOzux_Xh-URtLPeU8Ey6LfP29l9C',
+        '300-700': 'https://discord.com/api/webhooks/895274898329567232/2LI7Cmkxe7qj0B2SHPeCBCowUFhgqYLhLmw-SpP8xLuwu7dWhCYGb9B4rIJkFfcMv8pK',
+        '700-1,5млн': 'https://discord.com/api/webhooks/895274802556842004/lIxs3DlT38zbXXNnTzaP8ClsjEVcxxwh31BqGA8IePM5dvsi7XdhAtW4Fq9fS6377zy1',
+        '1,5млн>': 'https://discord.com/api/webhooks/895274725582962748/8F9tUBo0uU1ToQwXoapZPj7AApQ86PV7tORStpJjyZSZwc8GwaNNVD-OOrcttb47gh4A'
+    },
+    'Ростов-на-дону': {
+        'до 300к': 'https://discord.com/api/webhooks/895274481650663435/apck1CLMGczwSQu2RYD8j8J-czMZ8bXweKHD2n1J_iR8ybOzxJy_4RgjiESGGMPtCWSH',
+        '300-700': 'https://discord.com/api/webhooks/895274404618072074/IW2dr1gejZtp3YahpD-oCy6AZ1run3PvrocwkAp914JOHa3uIW3A9Qc_HkQpX554kAoV',
+        '700-1,5млн': 'https://discord.com/api/webhooks/895274278260457514/G9SyhBuwWBn-VT-a03Vha4O86aqT0SPnVSY-eCRzcwQ4JeXK40iFaR3gmnaDfBDXbIgD',
+        '1,5млн>': 'https://discord.com/api/webhooks/895274163269410856/tS4clXek0i_Q45XVlrYZWAsXbamBHtnfTqZ6FCHy9ZxQ9s2kPoOPvyghgjgvc99KQ10q'
+    },
+    'Самара': {
+        'до 300к': 'https://discord.com/api/webhooks/895273910659067904/J5ANGHj8IGT5XA4HeKK2kb9V7chxRn2iblI5ptvB8S4UZqc1auX6oXxLtqLXV4xUmevv',
+        '300-700': 'https://discord.com/api/webhooks/895273809224011846/2_Uprfl3rbN9338IUfyUu-czUecIVG6pG6slCAy5u_sgNpHWPSHvaum_yJRgMIZkJBZS',
+        '700-1,5млн': 'https://discord.com/api/webhooks/895273715498106902/0XiAdPWGq9Bu9nZjZmNHtMGSowYVgjVCei-j5ENu40Qi1vRlj-e2AT8yRdAynkCbaxmG',
+        '1,5млн>': 'https://discord.com/api/webhooks/895273625156993035/6g5RH-J5ZPjcXcO-ZywG3i-ewTXI5CVzwcQWnjmGPjStqwRJ9SaioTo84g7XAbk4suqh'
+    },
+    'Омск': {
+        'до 300к': 'https://discord.com/api/webhooks/895273506097483876/jQgUyTjsuWv8F53M4pClIy7JJBwqAkG6wMQWZn8MpBW5Drr4M7uInZeqBF1LFNXIhTaF',
+        '300-700': 'https://discord.com/api/webhooks/895273411310395412/kn5KtRdr4o9iqnfUOjim2VSJBRxYOOo6ZjiWlXtmCH7gDXZXXdI82jRP0c6SZYvfvQkW',
+        '700-1,5млн': 'https://discord.com/api/webhooks/895273317991333941/41W7PCxpGXkSUjQCRB5-Mg2_swgZhgI63yBwB408t8pGlQ3ok_FtQjrnXwfENKR5JNxb',
+        '1,5млн>': 'https://discord.com/api/webhooks/895273190534840331/drXa0HJIz1tOyk3Cq0Dz5uv5rSt9Gu2XIYPR2EBo8ojKDkBn84KNWCqWoKdUp0HYi1Vh'
+    },
+    'ЕКБ': {
+        'до 300к': 'https://discord.com/api/webhooks/895273021894426665/XopIy4_TbxSaHDtJvpjydr1uilSBiStQfUCaELeRZ_QWJzsvBtMcQp0UvrkAc9wmhcRJ',
+        '300-700': 'https://discord.com/api/webhooks/895272908153303100/WHHt961CgT4af1J81ftkCpTF-_nyCRcma02EPqskSmwwOSSO03leFHs9W8CoUQhf0fZR',
+        '700-1,5млн': 'https://discord.com/api/webhooks/895272793229373460/W4t5v4q6mtsSs_L9jLLIK1QO1lHTol7WULody9BDXw1J4ZJW7CN3YK_M6pU6OOhL2kaG',
+        '1,5млн>': 'https://discord.com/api/webhooks/895272687583236097/IM0h0lH1-OPIeGuxLMLne6nHl26Qat7dAyeK28_Su4cCF6mGcVvyRkzK7QcWZnP0nxwq'
+    },
+    'Казань': {
+        'до 300к': 'https://discord.com/api/webhooks/895272541717938196/-f8mh1g-HbzgIj5ZvDSsUTjelhFRawzlVgKxaKkdK2KgScThf1rbS4zH1hG6J6RU0IcT',
+        '300-700': 'https://discord.com/api/webhooks/895272419277807647/p5eigNdJpHk3h9bdqHqh6HWHrBqNR7JwHUgtpT25Lg-UtQpH88_gUAeRB0rDMXog_FpB',
+        '700-1,5млн': 'https://discord.com/api/webhooks/895272274037456897/1qnT5S8vjvhI40pWOxzqYUXChSDpKdXBoIds1tKGN7Rda85fvEs44HppwLGQkRJKiEPr',
+        '1,5млн>': 'https://discord.com/api/webhooks/895271472107515915/ll2z7VTfLkicye3AhxbJW5lWKExjrUKyBmjQxj2HaRrdWqJkK4AjEdbt4VBxDBbJMkhM'
+    },
+}
